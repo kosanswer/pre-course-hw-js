207f114ae07f826f00d395c7cded5116ed2437a7
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2023-07-25 15:03:10 +0300

    Добавлен стиль CSS и установлен желтый фон

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -4,5 +4,10 @@ let number1 = Math.floor(Math.random() * 100)
 «[вставь сюда полученное число] :Это четное число» или «[вставь сюда полученное число] :Это нечетное число». 
 С каждой перезагрузкой страницы у тебя будет новое число.​
 */
-let number1 = Math.floor(Math.random() * 100)
-console.log(number1);
+let number1 = Math.floor(Math.random() * 100);
+
+if (number1 % 2 === 0) {
+console.log(number1 + ": Это четное число");
+} else {
+console.log(number1 + ": Это нечетное число");
+}
