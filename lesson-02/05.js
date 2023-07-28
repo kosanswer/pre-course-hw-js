c1dfcc5e0eaea8ef7c9fccda5522b4d7568c41f3
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2023-07-28 10:27:28 +0300

    lesson02_exercise_05

---
+++ lesson-02/05.js
@@ -0,0 +1,10 @@
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passportCopy = Object.assign({}, passport); 
+passportCopy.name = "Ivan"; 
+
+console.log(passport.name); 
+console.log(passportCopy.name); 
